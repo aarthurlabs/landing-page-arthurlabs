1dfce2638c76ebd22a986cb6f4ae78d8feb80890
arthurlabs <contato.arthurlabs@gmail.com>
2026-09-20 18:55:28 -0300

    configure pnpm workspace

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  images: {
-    dangerouslyAllowSVG: true,
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-  },
-};
+    images: {
+        dangerouslyAllowSVG: true,
+        contentSecurityPolicy:
+            "default-src 'self'; script-src 'none'; sandbox;",
+    },
+}
 
-export default nextConfig;
+export default nextConfig
